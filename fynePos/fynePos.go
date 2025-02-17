fca792c8d3c0eb75862687bee01c793629a1cea2
Ivan Belov <dionis.krasava@gmail.com>
2025-02-17 12:33:57 +0700

    Вроде рабочий код

--- fynePos/fynePos.go
+++ fynePos/fynePos.go
@@ -1,73 +1,64 @@
 package fynepos
 
 import (
-	"fmt"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
+const (
+	WIDTH_WINDOW  = 400
+	HEIGHT_WINDOW = 300
+)
+
 func App() {
 	a := app.New()
-	w := a.NewWindow("apppppplication")
-	w.Resize(fyne.NewSize(500, 500))
+	w := a.NewWindow("Table with Labels and Entries")
+	w.Resize(fyne.NewSize(WIDTH_WINDOW, HEIGHT_WINDOW))
 
-	data := []string{"A", "B", "C", "Холин"}
+	// Данные для меток в первом столбце
+	labels := []string{"A", "B", "C", "Холин"}
+	labelsUM := []string{"mcg", "mg", "mg", "mg"}
 
+	// Создаем таблицу с двумя столбцами
 	table := widget.NewTable(
 		func() (int, int) {
-			return len(data), 1
+			return len(labels), 3 // 2 столбца: один для меток, один для полей ввода
 		},
-
 		func() fyne.CanvasObject {
-			return widget.NewLabel("Default text")
+			// Возвращаем контейнер, который может содержать либо Label, либо Entry
+			return container.NewStack(widget.NewLabel(""), widget.NewEntry())
 		},
-
 		func(tci widget.TableCellID, co fyne.CanvasObject) {
-			co.(*widget.Label).SetText(data[tci.Row])
+			// Настройка содержимого ячеек
+			stack := co.(*fyne.Container)
+			if tci.Col == 0 {
+				// Первый столбец: метки
+				label := stack.Objects[0].(*widget.Label)
+				label.SetText(labels[tci.Row])
+				label.Show()
+				stack.Objects[1].(*widget.Entry).Hide()
+			} else if tci.Col == 1 {
+				// Второй столбец: поля ввода
+				entry := stack.Objects[1].(*widget.Entry)
+				entry.SetPlaceHolder("0")
+				entry.Show()
+				stack.Objects[0].(*widget.Label).Hide()
+			} else if tci.Col == 2 {
+				label := stack.Objects[0].(*widget.Label)
+				label.SetText(labelsUM[tci.Row])
+				label.Show()
+				stack.Objects[1].(*widget.Entry).Hide()
+			}
 		},
 	)
 
-	table2 := widget.NewTable(
-		func() (int, int) {
-			return len(data), 1
-		},
+	// Устанавливаем ширину столбцов
+	table.SetColumnWidth(0, 75) // Первый столбец шире (200 пикселей)
+	table.SetColumnWidth(1, 50) // Второй столбец уже (100 пикселей)
 
-		func() fyne.CanvasObject {
-			entry := widget.NewEntry()
-			entry.SetPlaceHolder("0")
-			// Устанавливаем минимальную ширину для Entry
-			return container.NewHBox(container.NewGridWrap(fyne.NewSize(75, entry.MinSize().Height), entry))
-		},
-
-		func(tci widget.TableCellID, co fyne.CanvasObject) {
-			//co.(*widget.Label).SetText(data[tci.Row])
-		},
-	)
-
-	table3 := widget.NewTable(
-		func() (int, int) {
-			return len(data), 1
-		},
-
-		func() fyne.CanvasObject {
-			return widget.NewLabel("mg")
-		},
-
-		func(tci widget.TableCellID, co fyne.CanvasObject) {
-
-		},
-	)
-
-	cont := container.NewAdaptiveGrid(3, table, table2, table3)
-
-	w.SetContent(cont)
-
-	widthOneTable := table.Size()
-	fmt.Println(widthOneTable)
-
+	w.SetContent(table)
 	w.Show()
 	a.Run()
 }
