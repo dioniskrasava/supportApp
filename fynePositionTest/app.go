04b0b03b2ee89c3df00df3560bd37a8ab65a5a13
Ivan Belov <dionis.krasava@gmail.com>
2025-02-18 16:44:41 +0700

    поигрался с формами в файн

--- fynePositionTest/app.go
+++ fynePositionTest/app.go
@@ -1,30 +1,47 @@
 package fynepositiontest
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
 
 func App() {
 	a := app.New()
 	w := a.NewWindow("Grid Layout")
+	w.Resize(fyne.NewSize(300, 300))
 
-	// Создаем виджеты
-	button1 := widget.NewButton("Button 1", nil)
-	button2 := widget.NewButton("Button 2", nil)
-	button3 := widget.NewButton("Button 3", nil)
+	w1 := widget.NewEntry()
+	w2 := widget.NewEntry()
+	w3 := widget.NewEntry()
 
-	// Создаем сетку с тремя столбцами
-	grid := layout.NewGridLayout(3) // 3 столбца
+	item1 := widget.NewFormItem("A", w1)
+	item2 := widget.NewFormItem("B", w2)
+	item3 := widget.NewFormItem("C", w3)
 
-	// Устанавливаем ширину столбцов
-	grid.SetColumnWidths([]int{2, 2, 1}) // Ширина столбцов: 2, 2, 1
+	form := widget.NewForm(item1, item2, item3)
 
-	// Добавляем виджеты в сетку
-	content := container.New(grid, button1, button2, button3)
+	//form.OnSubmit = func() {}
+	//form.OnCancel = func() {}
+	//------------------------------------
 
-	w.SetContent(content)
+	w4 := widget.NewEntry()
+	w5 := widget.NewEntry()
+	w6 := widget.NewEntry()
+
+	item4 := widget.NewFormItem("D", w4)
+	item5 := widget.NewFormItem("E", w5)
+	item6 := widget.NewFormItem("U", w6)
+
+	form2 := widget.NewForm(item4, item5, item6)
+
+	//form2.OnSubmit = func() {}
+	//form2.OnCancel = func() {}
+	//-------------------------------------
+
+	globalcont := container.NewHBox(container.NewVBox(form), container.NewVBox(form2))
+
+	w.SetContent(globalcont)
 	w.ShowAndRun()
 }
