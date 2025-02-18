971fc100cf2cd75f6330254c319292f12b687735
Ivan Belov <dionis.krasava@gmail.com>
2025-02-18 13:37:27 +0700

    +

--- fixact/app.go
+++ fixact/app.go
@@ -20,10 +20,19 @@ type Activity struct {
 	Comment   string
 }
 
+// структура виджетов микроприложения
+type Widgets struct {
+	activityType *widget.Select
+	startTime    *widget.Entry
+	endTime      *widget.Entry
+	totalTime    *widget.Entry
+	comment      *widget.Entry
+	addButton    *widget.Button
+}
+
 func App() {
-	// Инициализация приложения Fyne
 	myApp := app.New()
-	myWindow := myApp.NewWindow("Activity Tracker")
+	myWindow := myApp.NewWindow("FixAct")
 
 	// Подключение к базе данных SQLite
 	db, err := sql.Open("sqlite3", "./fixact/activities.db")
@@ -52,31 +61,7 @@ func App() {
 	endTime := widget.NewEntry()
 	totalTime := widget.NewEntry()
 	comment := widget.NewMultiLineEntry()
-	addButton := widget.NewButton("Добавить активность", func() {
-		activity := Activity{
-			Type:      activityType.Selected,
-			StartTime: startTime.Text,
-			EndTime:   endTime.Text,
-			TotalTime: totalTime.Text,
-			Comment:   comment.Text,
-		}
-
-		// Вставка данных в базу данных
-		insertSQL := `INSERT INTO activities (type, start_time, end_time, total_time, comment) VALUES (?, ?, ?, ?, ?)`
-		_, err := db.Exec(insertSQL, activity.Type, activity.StartTime, activity.EndTime, activity.TotalTime, activity.Comment)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Очистка полей после добавления
-		activityType.SetSelected("")
-		startTime.SetText("")
-		endTime.SetText("")
-		totalTime.SetText("")
-		comment.SetText("")
-
-		fmt.Println("Активность добавлена!")
-	})
+	addButton := widget.NewButton("Добавить активность", func() { addAct() })
 
 	// Создание контейнера с элементами интерфейса
 	content := container.NewVBox(
@@ -97,4 +82,30 @@ func App() {
 	myWindow.SetContent(content)
 	myWindow.Resize(fyne.NewSize(400, 300))
 	myWindow.ShowAndRun()
+}
+
+func addAct() {
+	activity := Activity{
+		Type:      activityType.Selected,
+		StartTime: startTime.Text,
+		EndTime:   endTime.Text,
+		TotalTime: totalTime.Text,
+		Comment:   comment.Text,
+	}
+
+	// Вставка данных в базу данных
+	insertSQL := `INSERT INTO activities (type, start_time, end_time, total_time, comment) VALUES (?, ?, ?, ?, ?)`
+	_, err := db.Exec(insertSQL, activity.Type, activity.StartTime, activity.EndTime, activity.TotalTime, activity.Comment)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Очистка полей после добавления
+	activityType.SetSelected("")
+	startTime.SetText("")
+	endTime.SetText("")
+	totalTime.SetText("")
+	comment.SetText("")
+
+	fmt.Println("Активность добавлена!")
 }
